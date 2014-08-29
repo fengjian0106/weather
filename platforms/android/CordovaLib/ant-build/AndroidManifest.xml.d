f985b752cd72f244cf8be4caf627695a666aefc5
fengjian <fengjian0106@gmail.com>
2014-08-29 15:37:13 +0800

    remove platforms folder

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Kenshin/Desktop/zpWeather/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/Kenshin/Desktop/zpWeather/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/Kenshin/Desktop/zpWeather/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
